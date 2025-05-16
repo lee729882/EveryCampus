d1db978ed473554659836b036fa50079a97760c7
이승철 <@DESKTOP-HPINOGA>
2025-05-16 18:42:26 +0900

    fix: setFrom() 누락으로 발생한 Gmail 555 SMTP 오류 수정

--- everycampus/src/main/java/com/everycampus/service/MailService.java
+++ everycampus/src/main/java/com/everycampus/service/MailService.java
@@ -3,7 +3,11 @@ package com.everycampus.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 @RequiredArgsConstructor
@@ -13,9 +17,11 @@ public class MailService {
 
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("lee7298821@gmail.com"); // application.properties 이메일과 동일해야 함 !
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
         mailSender.send(message);
     }
+
 }
